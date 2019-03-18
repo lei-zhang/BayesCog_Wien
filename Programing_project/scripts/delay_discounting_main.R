34cb1b5b5ba6a4702195ba65ac817f036e56b959
lei-zhang <zhang.ninhamburg@gmail.com>
2019-03-18 18:54:54 +0100

    programing project

--- Programing_project/scripts/delay_discounting_main.R
+++ Programing_project/scripts/delay_discounting_main.R
@@ -14,38 +14,38 @@ library(loo)
 library(ggplot2)
 
 #### read raw -----------------------------------------------------------------
-rawdata   <- read.table( 'data/raw_Data.txt', header = T )
+rawdata   = # complete this line for reading raw data
 
 #### Preprocess the data ------------------------------------------------------
-subjList  <- unique(rawdata[,"subjID"])
-nSubjects <- length(subjList) 
+subjList  = unique(rawdata[,"subjID"])
+nSubjects = length(subjList) 
 
-Tsubj <- as.vector( rep( 0, nSubjects ) ) # number of valid trials per subj
+Tsubj = as.vector( rep( 0, nSubjects ) ) # number of valid trials per subj
 
 for ( s in 1:nSubjects )  {
-    curSubj  <- subjList[ s ]
-    Tsubj[s] <- sum( rawdata$subjID == curSubj )
+    curSubj  = subjList[ s ]
+    Tsubj[s] = sum( rawdata$subjID == curSubj )
 }
 
-maxTrials <- max(Tsubj)
-delay_later   <- array(0, c(nSubjects, maxTrials) )
-amount_later  <- array(0, c(nSubjects, maxTrials) )
-delay_sooner  <- array(0, c(nSubjects, maxTrials) )
-amount_sooner <- array(0, c(nSubjects, maxTrials) )
-choice <- array(0, c(nSubjects, maxTrials) )
+maxTrials = max(Tsubj)
+delay_later   = array(0, c(nSubjects, maxTrials) )
+amount_later  = array(0, c(nSubjects, maxTrials) )
+delay_sooner  = array(0, c(nSubjects, maxTrials) )
+amount_sooner = array(0, c(nSubjects, maxTrials) )
+choice = array(0, c(nSubjects, maxTrials) )
 
 for (s in 1:nSubjects) {
-    curSubj      <- subjList[s]
-    useTrials    <- Tsubj[s]
-    tmp          <- subset(rawdata, rawdata$subjID == curSubj)
-    delay_later[s, 1:useTrials]  <- tmp$delay_later
-    amount_later[s, 1:useTrials] <- tmp$amount_later
-    delay_sooner[s, 1:useTrials]  <- tmp$delay_sooner
-    amount_sooner[s, 1:useTrials] <- tmp$amount_sooner
-    choice[s, 1:useTrials] <- tmp$choice
+    curSubj      = subjList[s]
+    useTrials    = Tsubj[s]
+    tmp          = subset(rawdata, rawdata$subjID == curSubj)
+    delay_later[s, 1:useTrials]  = tmp$delay_later
+    amount_later[s, 1:useTrials] = tmp$amount_later
+    delay_sooner[s, 1:useTrials]  = tmp$delay_sooner
+    amount_sooner[s, 1:useTrials] = tmp$amount_sooner
+    choice[s, 1:useTrials] = tmp$choice
 }
 
-dataList <- list(
+dataList = list(
     nSubjects = nSubjects,
     nTrials   = maxTrials,
     Tsubj     = Tsubj,
@@ -62,46 +62,32 @@ dataList <- list(
 rstan_options(auto_write = TRUE)
 options(mc.cores = 2)
 
-nIter     <- 2000
-nChains   <- 4 
-nWarmup   <- floor(nIter/2)
-nThin     <- 1
+nIter     = 2000
+nChains   = 4 
+nWarmup   = floor(nIter/2)
+nThin     = 1
 
 #### run the hyperbolic model ----------------------------------------
-modelFile1 <- 'scripts/hyperbolic.stan'
+modelFile1 = 'scripts/hyperbolic.stan'
 
 cat("Estimating", modelFile1, "model... \n")
 startTime = Sys.time(); print(startTime)
 cat("Calling", nChains, "simulations in Stan... \n")
 
-fit_dd1 <- stan(modelFile1, 
-               data    = dataList, 
-               chains  = nChains,
-               iter    = nIter,
-               warmup  = nWarmup,
-               thin    = nThin,
-               init    = "random"
-)
+fit_hyperbolic = stan() # complete this line for calling Stan
 
 cat("Finishing", modelFile1, "model simulation ... \n")
 endTime = Sys.time(); print(endTime)  
 cat("It took",as.character.Date(endTime - startTime), "\n")
 
 #### run the simple heuristic model ---------------------------------------
-modelFile2 <- 'scripts/heuristic.stan'
+modelFile2 = 'scripts/heuristic.stan'
 
 cat("Estimating", modelFile1, "model... \n")
 startTime = Sys.time(); print(startTime)
 cat("Calling", nChains, "simulations in Stan... \n")
 
-fit_dd2 <- stan(modelFile2, 
-               data    = dataList, 
-               chains  = nChains,
-               iter    = nIter,
-               warmup  = nWarmup,
-               thin    = nThin,
-               init    = "random"
-)
+fit_heuristic = stan() # complete this line for calling Stan
 
 cat("Finishing", modelFile2, "model simulation ... \n")
 endTime = Sys.time(); print(endTime)  
@@ -110,12 +96,11 @@ cat("It took",as.character.Date(endTime - startTime), "\n")
 # =============================================================================
 #### Model selection #### 
 # =============================================================================
-LL1 = extract_log_lik(fit_dd1)
-LL2 = extract_log_lik(fit_dd2)
+LL_hyperbolic = # complete this line for extreact log-likelihood
+LL_heuristic  = # complete this line for extreact log-likelihood
 
-( loo1 <- waic(LL1) )
-( loo2 <- waic(LL2) )
+waic_hyperbolic = waic(LL1)
+waic_heuristic  = waic(LL2)
 
 
 #### End of file
-
